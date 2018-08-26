f7b2670b989fff2b49252dcd805db2dfe4b9db01
Gidong Kwon <lucidfext@gmail.com>
2018-08-26 17:00:06 +0900

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
